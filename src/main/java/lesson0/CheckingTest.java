a27ef83eaf48708d572f85d98031d0ab86c1b05f
Roma <roma.sk.ukr@gmail.com>
2023-07-02 12:08:52 +0300

    Initial commit

---
+++ src/main/java/lesson0/CheckingTest.java
@@ -0,0 +1,21 @@
+package lesson0;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CheckingTest {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver","C:\\selenium\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://guinnessworldrecords.com/records/");
+        Thread.sleep(3000);
+        System.out.println(driver.getCurrentUrl());
+        driver.quit();
+
+
+
+    }
+}
