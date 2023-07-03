74646105b3516f4480e22795f3a778ea0b5be9fc
Roma <roma.sk.ukr@gmail.com>
2023-07-03 22:09:07 +0300

    new

--- src/main/java/lesson2/By_xpath_text.java
+++ src/main/java/lesson2/By_xpath_text.java
@@ -14,5 +14,19 @@ public class By_xpath_text {
         Thread.sleep(2000);
         System.out.println(driver.findElement(By.xpath("//a[text() = 'Standard application']")).getText());
         driver.quit();
+
+         /*
+        /.. вверх по дереву
+        /tag_name вниз к указанному тегу
+        /parent::tag_name переход на родительский тег
+        /child::tag_name переход на дочерний тег
+        //following-sibling::tag_name перход на параллельный тег
+         */
+
+/*        driver.get("https://www.guinnessworldrecords.com/records/apply-to-set-or-break-a-record/");
+        Thread.sleep(2000);
+        driver.findElement(By.id("ez-accept-all")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[text()='APPLY']/../following-sibling::div[3]")).click();*/
     }
 }
