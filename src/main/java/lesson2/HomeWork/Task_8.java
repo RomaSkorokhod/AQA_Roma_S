91329b7175e4695b94d377de01767f0844b83796
Roma <roma.sk.ukr@gmail.com>
2023-07-09 00:38:15 +0300

    new

--- src/main/java/lesson2/HomeWork/Task_8.java
+++ src/main/java/lesson2/HomeWork/Task_8.java
@@ -6,7 +6,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 /*Отыскать один элемент на основе другого.
         Для нахождения первого элемента можете использовать любые методы и локаторы,
-        для прописывания дальнейшего поиска второго элемента не использовать текст.*/
+        для прописывания дальнейшего поиска второго элемента не использовать текст.
+        P.S. Не получается сделать это задание. Если есть возможность, отправте пожалуйста
+        код, попробую сам разобратся. Спасибо
+        */
 public class Task_8 {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver.exe");
