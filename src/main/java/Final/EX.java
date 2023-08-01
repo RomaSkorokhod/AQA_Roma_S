45a707eeca6b7ff24b08bb656ddb576ea632d314
Roma <roma.sk.ukr@gmail.com>
2023-08-02 02:05:04 +0300

    new

--- src/main/java/Final/EX.java
+++ src/main/java/Final/EX.java
@@ -1,8 +1,6 @@
 package Final;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
@@ -24,15 +22,13 @@ public class EX {
         driver.manage().window().maximize();
         driver.get("https://auto.ria.com/uk/");
         driver.findElement(By.xpath("//label[@class='js-close c-notifier-btn']")).click();
-
-        driver.findElement(By.xpath("//label[@for='forPrice']")).click();
-        driver.findElement(By.id("priceFrom")).sendKeys("2000");
-        driver.findElement(By.id("priceTo")).sendKeys("40000");
+        ((JavascriptExecutor)driver).executeScript("");
+        driver.get("https://zapchasti.ria.com/uk/");
 
 
-
-        driver.findElement(By.xpath("//div[@class='span8 form-search']")).click();
-        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        WebElement element = driver.findElement(By.xpath("//input[@class='flex-input autocomplete__input']"));
+        element.sendKeys("Лампа розжарювання C10W 12V 10W BOSCH");
+        element.sendKeys(Keys.ENTER);
 
 
 
--- src/main/java/Final/Test3.java
+++ src/main/java/Final/Test3.java
@@ -1,9 +1,11 @@
 package Final;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Test3 {
 
@@ -14,10 +16,38 @@ public class Test3 {
         WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get("https://auto.ria.com/uk/");
-
         driver.findElement(By.xpath("//label[@class='js-close c-notifier-btn']")).click();
+        ((JavascriptExecutor)driver).executeScript("");
+        driver.get("https://zapchasti.ria.com/uk/");
 
-        driver.findElement(By.linkText("//a[@href='https://zapchasti.ria.com/uk/']")).click();
+        WebElement element = driver.findElement(By.xpath("//input[@class='flex-input autocomplete__input']"));
+        element.sendKeys("Лампа розжарювання C10W 12V 10W BOSCH");
+        element.sendKeys(Keys.ENTER);
+        driver.findElement(By.xpath("//h2[contains(text(),'BOSCH 1987302210')]")).click();
+        driver.findElement(By.xpath("//span[@class='js-buy-text']")).click();
+        driver.findElement(By.xpath("//a[@class='go-shop show-more-simple close-js']")).click();
+
+        WebElement element2 = driver.findElement(By.id("js-search-text"));
+        element2.sendKeys("форсунка для Volkswagen Beetle");
+        element2.sendKeys(Keys.ENTER);
+        driver.findElement(By.xpath("//h2[contains(text(),'Volkswagen Beetle 1.6TDI 03L130277b')]")).click();
+        driver.findElement(By.xpath("//span[@class='js-buy-text']")).click();
+        driver.findElement(By.xpath("//a[@class='go-shop show-more-simple close-js']")).click();
+
+        WebElement element3 = driver.findElement(By.id("js-search-text"));
+        element3.sendKeys("Мастило трансмісійне 1л 08886");
+        element3.sendKeys(Keys.ENTER);
+        driver.findElement(By.xpath("//h2[contains(text(),'Мастило трансмісійне 1л 08886')]")).click();
+        driver.findElement(By.xpath("//span[@class='js-buy-text']")).click();
+        driver.findElement(By.xpath("//a[@class='go-shop show-more-simple close-js']")).click();
+
+
+
+
+
+
+
+
 
 
 
