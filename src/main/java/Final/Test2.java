61876a59006ada10f136e1eea755ae603943184c
Roma <roma.sk.ukr@gmail.com>
2023-08-02 16:01:09 +0300

    new

--- src/main/java/Final/Test2.java
+++ src/main/java/Final/Test2.java
@@ -1,17 +1,10 @@
 package Final;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import utils.Waiters;
-
-import java.time.Duration;
 
 public class Test2 {
-    private static Waiters waiters;
 
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver.exe");
@@ -21,21 +14,63 @@ public class Test2 {
         driver.manage().window().maximize();
         driver.get("https://auto.ria.com/uk/");
         driver.findElement(By.xpath("//label[@class='js-close c-notifier-btn']")).click();
-        driver.findElement(By.xpath("//span[@class='tl']")).click();
+        ((JavascriptExecutor)driver).executeScript("");
+        driver.get("https://zapchasti.ria.com/uk/");
 
-        driver.switchTo().frame("login_frame");
-        driver.findElement(By.xpath("//a[contains(text(),'Зареєструватися на AUTO.RIA')]")).click();
 
-        driver.findElement(By.xpath("//form[@id='login-form']"));
-        driver.findElement(By.id("registrationform-name")).sendKeys("Roman");
-        driver.findElement(By.id("registrationform-second_name")).sendKeys("Shevchenko");
-        driver.findElement(By.id("registrationform-email")).sendKeys("Shevaukr.net");
-        driver.findElement(By.xpath("//label[@for='registrationform-agree']")).click();
+        WebElement element1 = driver.findElement(By.id("js-search-text"));
+        element1.sendKeys("форсунка для Volkswagen Beetle");
+        element1.sendKeys(Keys.ENTER);
+        driver.findElement(By.xpath("//h2[contains(text(),'Volkswagen Beetle 1.6TDI 03L130277b')]")).click();
+        driver.findElement(By.xpath("//span[@class='js-buy-text']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//a[@class='go-shop show-more-simple close-js']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[@title='Кошик']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//span[@class='ticket__remove']")).click();
+        Thread.sleep(1000);
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
-        driver.findElement(By.id("registration_button")).click();
+        driver.get("https://zapchasti.ria.com/uk/");
+        WebElement element2 = driver.findElement(By.id("js-search-text"));
+        element2.sendKeys("Мастило трансмісійне 1л 08886");
+        element2.sendKeys(Keys.ENTER);
+        driver.findElement(By.xpath("//h2[contains(text(),'Мастило трансмісійне 1л 08886')]")).click();
+        driver.findElement(By.xpath("//span[@class='js-buy-text']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//a[@class='go-shop show-more-simple close-js']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[@title='Кошик']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//span[@class='ticket__remove']")).click();
+        Thread.sleep(1000);
+
+        driver.get("https://zapchasti.ria.com/uk/");
+        WebElement element3 = driver.findElement(By.xpath("//input[@class='flex-input autocomplete__input']"));
+        element3.sendKeys("Лампа розжарювання C10W 12V 10W BOSCH");
+        element3.sendKeys(Keys.ENTER);
+        driver.findElement(By.xpath("//h2[contains(text(),'BOSCH 1987302210')]")).click();
+        driver.findElement(By.xpath("//span[@class='js-buy-text']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//a[@class='go-shop show-more-simple close-js']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[@title='Кошик']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[@class='button _green middle']")).click();
+        Thread.sleep(4000);
 
         driver.quit();
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
--- src/main/java/Final/Test3.java
+++ src/main/java/Final/Test3.java
@@ -1,13 +1,16 @@
 package Final;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.Waiters;
 
 import java.time.Duration;
 
 public class Test3 {
+    private static Waiters waiters;
 
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver.exe");
@@ -17,40 +20,21 @@ public class Test3 {
         driver.manage().window().maximize();
         driver.get("https://auto.ria.com/uk/");
         driver.findElement(By.xpath("//label[@class='js-close c-notifier-btn']")).click();
-        ((JavascriptExecutor)driver).executeScript("");
-        driver.get("https://zapchasti.ria.com/uk/");
+        driver.findElement(By.xpath("//span[@class='tl']")).click();
 
-        WebElement element = driver.findElement(By.xpath("//input[@class='flex-input autocomplete__input']"));
-        element.sendKeys("Лампа розжарювання C10W 12V 10W BOSCH");
-        element.sendKeys(Keys.ENTER);
-        driver.findElement(By.xpath("//h2[contains(text(),'BOSCH 1987302210')]")).click();
-        driver.findElement(By.xpath("//span[@class='js-buy-text']")).click();
-        driver.findElement(By.xpath("//a[@class='go-shop show-more-simple close-js']")).click();
+        driver.switchTo().frame("login_frame");
+        driver.findElement(By.xpath("//a[contains(text(),'Зареєструватися на AUTO.RIA')]")).click();
 
-        WebElement element2 = driver.findElement(By.id("js-search-text"));
-        element2.sendKeys("форсунка для Volkswagen Beetle");
-        element2.sendKeys(Keys.ENTER);
-        driver.findElement(By.xpath("//h2[contains(text(),'Volkswagen Beetle 1.6TDI 03L130277b')]")).click();
-        driver.findElement(By.xpath("//span[@class='js-buy-text']")).click();
-        driver.findElement(By.xpath("//a[@class='go-shop show-more-simple close-js']")).click();
+        driver.findElement(By.xpath("//form[@id='login-form']"));
+        driver.findElement(By.id("registrationform-name")).sendKeys("Roman");
+        driver.findElement(By.id("registrationform-second_name")).sendKeys("Shevchenko");
+        driver.findElement(By.id("registrationform-email")).sendKeys("Shevaukr.net");
+        driver.findElement(By.xpath("//label[@for='registrationform-agree']")).click();
 
-        WebElement element3 = driver.findElement(By.id("js-search-text"));
-        element3.sendKeys("Мастило трансмісійне 1л 08886");
-        element3.sendKeys(Keys.ENTER);
-        driver.findElement(By.xpath("//h2[contains(text(),'Мастило трансмісійне 1л 08886')]")).click();
-        driver.findElement(By.xpath("//span[@class='js-buy-text']")).click();
-        driver.findElement(By.xpath("//a[@class='go-shop show-more-simple close-js']")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        driver.findElement(By.id("registration_button")).click();
 
-
-
-
-
-
-
-
-
-
-
+        driver.quit();
 
     }
 }
